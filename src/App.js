fa4814d90919e5a9ca3e8508c5f60eabea441e18
Michał Grochalski <michal.grochalski@gmail.com>
2022-12-14 10:32:07 +0100

    init QR code Component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="qr-code">
+        <img src="images/image-qr-code.png" alt="QR code" />
+          <h1>Improve your front-end skills by building projects</h1>
+          <h2>Scan the QR code to visit Frontend Mentor and take your coding skils to the next level</h2>
+      </div>
     </div>
   );
 }
