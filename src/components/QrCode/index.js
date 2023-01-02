0f3f2348d41d746b0f36af95e6dcfecfc3627a46
Michał Grochalski <michal.grochalski@gmail.com>
2023-01-02 14:36:33 +0100

    Add BEM Css style

--- src/components/QrCode/index.js
+++ src/components/QrCode/index.js
@@ -1,10 +1,10 @@
 import './QrCode.scss';
 
 const QrCode = ({img, title, description}) => (
-        <div id="qr-code">
-            <img src={img} alt="QR code" />
-            <h1>{title}</h1>
-            <h2>{description}</h2>
+        <div className="qrCode">
+            <img src={img} className="qrCode__img" alt="QR code" />
+            <h1 className="qrCode__title">{title}</h1>
+            <h2 className="qrCode__description">{description}</h2>
         </div>
 )
 
