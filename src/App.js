dd252468127c042d048199c97feccbcd168b354f
Michał Grochalski <michal.grochalski@gmail.com>
2023-01-02 14:36:21 +0100

    Correct typo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     const config = {
         img: qrCodeImg,
         title: "Improve your front-end skills by building projects",
-        description: "Scan the QR code to visit Frontend Mentor and take your coding skils to the next level",
+        description: "Scan the QR code to visit Frontend Mentor and take your coding skills to the next level",
     }
 
     return (
