aa5865d4a9b737d5bc4f14a6929a494814817e01
Michał Grochalski <michal.grochalski@gmail.com>
2023-01-02 14:18:15 +0100

    Add implicit return in arrow function

--- src/components/QrCode/index.js
+++ src/components/QrCode/index.js
@@ -1,13 +1,11 @@
 import './QrCode.scss';
 
-const QrCode = ({img, title, description}) => {
-    return (
+const QrCode = ({img, title, description}) => (
         <div id="qr-code">
             <img src={img} alt="QR code" />
             <h1>{title}</h1>
             <h2>{description}</h2>
         </div>
-    )
-}
+)
 
 export default QrCode;
