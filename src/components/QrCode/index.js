3217441588a7c61d247349f31f19cf0ffd167787
Michał Grochalski <michal.grochalski@gmail.com>
2023-01-03 15:12:02 +0100

    Rename css classes

--- src/components/QrCode/index.js
+++ src/components/QrCode/index.js
@@ -1,10 +1,10 @@
 import styles from './QrCode.module.scss';
 
 const QrCode = ({img, title, description}) => (
-        <div className={styles.qrCode}>
-            <img src={img} className={styles.qrCode__img} alt="QR code" />
-            <h1 className={styles.qrCode__title}>{title}</h1>
-            <h2 className={styles.qrCode__description}>{description}</h2>
+        <div className={styles.card}>
+            <img src={img} className={styles.img} alt="QR code" />
+            <h1 className={styles.title}>{title}</h1>
+            <h2 className={styles.description}>{description}</h2>
         </div>
 )
 
